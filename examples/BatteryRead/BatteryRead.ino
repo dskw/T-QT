7c3fdeeb3978d28ed3571ab22fa53ef1765e7509
dskw <dskw@users.noreply.github.com>
2023-06-01 13:38:05 +0200

    Update BatteryRead.ino

--- examples/BatteryRead/BatteryRead.ino
+++ examples/BatteryRead/BatteryRead.ino
@@ -1,21 +1,42 @@
+#include <WiFi.h>
+#include <BluetoothSerial.h>
+#include "driver/adc.h"
+#include <esp_bt.h>
+
 #include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
 #include <SPI.h>
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 
 float battery_read()
 {
+  adc_power_on();
   float voltage = analogRead(4) * 3.3f / 4095.0f * 2.02;
-  //return voltage;
+  adc_power_off();
+  return voltage;
+}
+
+float battery_percent(float voltage)
+{
   return 123.0f - 123.0f / pow(1.0f + pow(voltage / 3.7f, 80), 0.165f);
+}
+
+void powerSafe()
+{
+  adc_power_off();
+  btStop(); // disable Bluetooth
+  WiFi.disconnect(true);  // Disconnect from the network
+  WiFi.mode(WIFI_OFF);    // Switch WiFi off
 }
 
 void setup(void) {
   tft.init();
   tft.setRotation(2);
+  powerSafe();
 }
 
 void loop() {
   float batVol = battery_read();
+  float batPercent = battery_percent(batVol);
     
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE);
@@ -23,7 +44,10 @@ void loop() {
   tft.setCursor(0, 0);
   tft.setTextFont(0);
   tft.println("Battery:");
-  tft.println(batVol);
+  tft.print(batPercent);
+  tft.println(" %");
+  tft.print(batVol);
+  tft.println(" V");
 
-  delay(1000);
+  delay(2000);
 }
